5942fb2c92fd74f9bf4118741fc52f92adbfa8cc
Rahaf Al-Jazzazi <rahafjazz@gmail.com>
2021-05-24 15:08:29 +0300

    editing the css code

--- js/app.js
+++ js/app.js
@@ -1,127 +1,18 @@
 'use strict';
-// comment for 1 line
 
-/*
-comments for multible lines
-Data types:
-- String
-any sympol inside 'single quotes' or "double quotes"
--Number
-5 integer , 2.5 floats
--Boolean
-true or false
-*/
+// Asking about the Name of the User , Age and favorite Song and Movie
 
-// let userName='samer';
+ let userName=prompt('Please wirte you Name here:');
+        console.log(userName);
 
-// console.log(userName);
+        let age=prompt('How old are you?');
+        console.log(age);
 
-// let age= prompt('whats your age?');
+        let favSong=prompt("What is you favorite song?")
+        console.log(favSong);
 
-// console.log(age);
+        let favMovie= prompt("What is your favorite movie")
+        console.log(favMovie)
 
-// check the data type of a variable:
-
-// console.log(typeof age);
-
-// change data type to a nuymber
-// age=Number(age);
-// age=parseInt(age);
-
-// console.log('after',typeof age);
-
-/*
-If statement:
-if(condition){
-    do this code if the condition is true
-}else if(second condition){
-    do this if second condition is true
-}else{
-    do this if all conditions are false
-}
-// */
-
-// let feeling=confirm('are you good?');
-
-// if (feeling===true){
-//     console.log('thats great to hear');
-// }else{
-//     console.log('hope you feel better soon');
-// }
-
-
-// let favouriteColor=prompt('what do you like more red/black?');
-
-// console.log('before',favouriteColor);
-
-// favouriteColor=favouriteColor.toLowerCase();
-
-
-// favouriteColor=favouriteColor.toUpperCase();
-
-// console.log('after',favouriteColor);
-
-// if (favouriteColor==='black') {
-//     console.log('you like black more');
-// }else if(favouriteColor==='red'){
-//     console.log('you like red more');
-// }else{
-//     console.log('please write red or black');
-// }
-
-/*
-Switch
-switch(variable){
-    case 'value 1':
-        do code..
-        break;
-    
-    case 'value2':
-        do code..
-        break;
-    default:
-        do if nothing matches the cases
-        break;
-}
-*/
-
-let fruit = prompt('what is your favourite fruit');
-
-switch (fruit.toUpperCase()) {
-    case 'APPLE':
-        console.log('apples are 5 dinars a kilo');
-        break;
-
-    case 'MELON':
-    case 'BANANA':
-        console.log('bananas and melons are 3 dinars a kilo');
-        break;
-
-    default:
-        console.log('sorry we dont have that');
-        break;
-}
-
-
-/*
-AND
-true && true=>true
-true &&false=>false
-false && true=>false
-false && false=>false
-OR
-true || true => true
-true || false =>true
-false|| true => true
-false || false => false
-*/
-
-
-/*
-GitHub
-The cloud
-store code online and share it with other developers
-Git
-VCS
-LOCAL machine
-*/
+        alert('Hope you have a good day '+ userName+ '! You are '+ age + ' years old. Welcome !');
+        alert("Your favorite Movie is "+ favMovie +". And you favorite song is "+favSong )
