8cea3d9256f141b8aa58726df6ab741e75163f60
Rahaf Al-Jazzazi <rahafjazz@gmail.com>
2021-05-26 13:41:14 +0300

    fixing the errors for question 6 and 7

--- js/app.js
+++ js/app.js
@@ -17,6 +17,7 @@ alert('Hope you have a good day ' + userName + '! You are ' + age + ' years old.
 //Start the Guessing Game
 let score = 0;
 
+// Question 1
 let favColor = prompt('Is my favorite Color Blue ?');
 favColor = favColor.toUpperCase()
 if (favColor == 'YES' || favColor == 'Y') {
@@ -28,6 +29,7 @@ if (favColor == 'YES' || favColor == 'Y') {
     alert('Wrong Asnwer, I like Blue')
 
 }
+// Question 2
 let favFood = prompt('Is my favourite food Pizza?!');
 favFood = favFood.toUpperCase()
 if (favFood == 'YES' || favFood == 'Y') {
@@ -39,6 +41,7 @@ if (favFood == 'YES' || favFood == 'Y') {
     score++
 
 }
+// Question 3
 let favHobby = prompt('Do you think playing Piano is my hobby?');
 favHobby = favHobby.toUpperCase()
 if (favHobby == 'YES' || favHobby == 'Y') {
@@ -50,6 +53,7 @@ if (favHobby == 'YES' || favHobby == 'Y') {
     alert('Oops, Wrong answer')
 
 }
+// Question 4
 let pet = prompt('Do i have a cat?');
 pet = pet.toUpperCase()
 if (pet == 'YES' || pet == 'Y') {
@@ -61,73 +65,102 @@ if (pet == 'YES' || pet == 'Y') {
     score++
 
 }
-let  favMovie= prompt('Do I like Movies?');
+// Question 5
+let favMovie = prompt('Do I like Movies?');
 favMovie = favMovie.toUpperCase()
 if (favMovie == 'YES' || favMovie == 'Y') {
     alert('You are correct! ');
     score++
-    
+
 
 } else if (favMovie == 'NO' || favMovie == 'N') {
     alert('No, you are wrong');
     //console.log('Really, are you Human?')
 
 }
+// Question 6
+// let travel = prompt('Do you think I like to Travel?');
+// travel = travel.toUpperCase()
+// if (travel == 'YES' || travel == 'Y') {
+//     alert('YES' + ' ' + userName + ' ' + ',everyone wants to travel! ');
+//     score++
+//     console.log('YES' + ' ' + userName + ' ' + ',everyone wants to travel! ')
 
-let travel = prompt('Do you think I like to Travel?');
-travel = travel.toUpperCase()
-if (travel == 'YES' || travel == 'Y') {
-    alert('YES' + ' ' + userName + ' ' + ',everyone wants to travel! ');
-    score++
-    //console.log('YES' + ' ' + userName + ' ' + ',everyone wants to travel! ')
+// } else if (travel == 'NO' || travel == 'N') {
+//     alert('Really, are you Human?');
+//     console.log('Really, are you Human?')
 
-} else if (travel == 'NO' || travel == 'N') {
-    alert('Really, are you Human?');
-    //console.log('Really, are you Human?')
+// }
+// let guessNumber = prompt("Guess how old am I ?")
+// while (!guessNumber) {
+//     guessNumber = prompt("Please guess my age")
+// }
+// for (let i = 0; i < 3; i++) {
 
-}
+//     if (guessNumber > 24) {
+//         alert("It is too high")
+//         guessNumber = prompt("Try Again")
+//     } else if (guessNumber < 24) {
+//         alert("It is too low")
+//         guessNumber = prompt("Try Again")
+//     } else if (guessNumber === 24) {
+//         score++
+//         alert("That\'s Right!")
+//         break
+//     }
+// }
 
-let guessNumber = prompt("Guess how old am I ?")
-while (!guessNumber) {
-    guessNumber = prompt("Please guess my age")
-}
-for (let i = 0; i < 3; i++) {
+// Quetion 6
 
-    if (guessNumber > 24) {
-        alert("It is too high")
-        guessNumber = prompt("Try Again")
-    } else if (guessNumber < 24) {
-        alert("It is too low")
-        guessNumber = prompt("Try Again")
-    } else if (guessNumber === 24) {
-        alert("That\'s Right!")
-        score++
-        break
+for (let i = 0; i < 4; i++) {
+    let guessNumber = Number(prompt('Guess how old am I ?'));
+    let myAge = 24;
+    if (guessNumber === myAge) {
+        alert("That\'s Right!");
+        score++;
+        break;
+    } else if (guessNumber > myAge) {
+        alert('too high');
+    } else if (guessNumber < myAge) {
+        alert('too low');
+    } else {
+        alert('Next time enter a number');
     }
+    if (i === 3) {
+        alert('I\'m 24 :D ');
+    }
+    console.log(i);
+}
+//Quetion 7
+
+let movieType = ['action', 'drama', 'romantic', 'adventure', 'comedy', 'animation'];
+
+
+
+let typesOfMovies = ""
+
+// while (!typesOfMovies) {
+//     typesOfMovies = prompt("Please guess my best movie type: 'Action','Drama','Romantic','Adventure','Comedy','Animation")
+// }
+
+
+q7: for (let i = 0; i < 6; i++) {
+
+    typesOfMovies = prompt("Please guess my best movie type: 'Action','Drama','Romantic','Adventure','Comedy','Animation")
+
+    for (let j = 0; j < movieType.length; j++) {
+        console.log('type', movieType[j])
+        if (movieType[j] == typesOfMovies) {
+            score++
+            alert("That\'s True! I like this type too.")
+
+            break q7
+        }
+
+    }
+
 }
 
-let movieType=['action','drama','romantic','adventure','comedy','animation'];
-
-
-let typesOfMovies=prompt("Please guess my best movie type: 'Action','Drama','Romantic','Adventure','Comedy','Animation'")
-
-while (!typesOfMovies) {
-    typesOfMovies = prompt("Best type of movies?")
- }
-
- for (let i = 0; i < 6; i++) {
-
-    for(let j=0; j < movieType.length;j++){
-        
-      if (movieType[j] == typesOfMovies){
-        console.log('type',movieType[j])
-        score++
-        break
-      }
-      
-    }
-    
-  }
 
 
 alert(`${userName}, your score is  ${score}/7.  Thanks for playing! `);
